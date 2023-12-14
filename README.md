e6de60c15f8b49e0c4102c73080d0d283632d2cd
nisha.tade <nisha.tade@sufalamtech.com>
2023-12-14 11:55:15 +0530

    nishu's chat app

---
+++ README.md
@@ -0,0 +1,32 @@
+## Available Scripts
+
+There is backend and frontend both in this project.
+
+
+## NODE Side
+
+### `npm i`
+
+Install all the packages.\
+Create .env file if not there and copy paste the content of env.txt in it.
+
+### `npm start`
+
+Runs the node server on port 5000. Keep the server running.
+
+
+## REACT Side
+
+### `npm i`
+
+Install all the packages in frontend folder. use (cd frontend) command first than (npm i).\
+If npm i does not works than use (npm i -f).
+
+### `npm start`
+
+Runs the app.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+
+
---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,62 @@
+const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
+
+//@description     Get all Messages
+//@route           GET /api/Message/:chatId
+//@access          Protected
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+//@description     Create New Message
+//@route           POST /api/Message/
+//@access          Protected
+const sendMessage = asyncHandler(async (req, res, next) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    return res.sendStatus(400);
+  }
+
+  const newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    let message = await Message.create(newMessage);
+
+    // Use populate with execPopulate method on the sender and chat fields
+    message = await Message.populate(message, [
+      { path: "sender", select: "name pic" },
+      { path: "chat" },
+    ]);
+
+    // Populate the chat.users field of the message
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    // Update the latestMessage field in the Chat model
+    await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
+
+    res.json(message);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = { allMessages, sendMessage };
